1d72a85b95504e00571ac2f754be39355f2f108e
Ana Lívia Ruegger Saldanha <analiviaruegger@gmail.com>
2022-05-13 23:46:20 -0300

    Fix memory leaks

--- E10/E10.cpp
+++ E10/E10.cpp
@@ -1,63 +1,32 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-void check_grid(char **original, char **copy, char *word, int index, int i, int j, bool *found_word);
-char **copy_grid(char **original_grid);
+typedef vector<char> vector_t;
+typedef vector<vector_t> matrix_t;
 
 int n, m;
 int k;
 
-int main()
+matrix_t copy_grid(matrix_t original_grid)
 {
-    cin >> n >> m;
-    getchar(); // Consome o '\n'
-
-    char **grid = (char **) malloc(n * sizeof(char *));
+    vector_t row(m);
+    matrix_t copy;
 
     for (int i = 0; i < n; i++)
     {
-        grid[i] = (char *) malloc(m * sizeof(char));
-        
         for (int j = 0; j < m; j++)
-            scanf("%c", &grid[i][j]);
-        
-        getchar(); // Consome o '\n'
+            row[j] = original_grid[i][j];
+        copy.push_back(row);
     }
 
-    cin >> k;
-    getchar(); // Consome o '\n'
-
-    char *word = (char *) malloc(k * sizeof(char));
-    for (int i = 0; i < k; i++)
-        scanf("%c", &word[i]);
-    
-    bool found_word = false;
-    char **copy = NULL;
-    check_grid(grid, copy, word, 0, 0, 0, &found_word);
-
-    if (found_word) printf("Yes\n");
-    else printf("No\n");
-
-    return 0;
+    return copy;
 }
 
-void check_grid(char **original, char **copy, char *word, int index, int i, int j, bool *found_word)
+void _check_grid(matrix_t original, matrix_t copy, vector_t word, int index, int i, int j, bool *found_word)
 {
-    if (!copy) // É a primeira chamada
-    {
-        for (int i = 0; i < n; i++)
-        {
-            for (int j = 0; j < m; j++)
-            {
-                copy = copy_grid(original);
-                check_grid(original, copy, word, index + 1, i, j, found_word);
-            }
-        }
-    }
-
     char c;
-
     if (index < k && i >= 0 && i < n && j >= 0 && j < m) c = word[index];
     else return;
 
@@ -76,31 +45,51 @@ void check_grid(char **original, char **copy, char *word, int index, int i, int 
     {
         copy[i][j] = '.';
         
-        char **new_copy = copy_grid(copy);
-        check_grid(original, new_copy, word, index + 1, i - 1, j, found_word); // Cima (i - 1, j)
-        
-        new_copy = copy_grid(copy);
-        check_grid(original, new_copy, word, index + 1, i + 1, j, found_word); // Baixo (i + 1, j)
-        
-        new_copy = copy_grid(copy);
-        check_grid(original, new_copy, word, index + 1, i, j - 1, found_word); // Esquerda (i, j - 1)
-        
-        new_copy = copy_grid(copy);
-        check_grid(original, new_copy, word, index + 1, i, j + 1, found_word); // Direita (i, j + 1)
+        _check_grid(original, copy_grid(copy), word, index + 1, i - 1, j, found_word); // Cima (i - 1, j)
+        _check_grid(original, copy_grid(copy), word, index + 1, i + 1, j, found_word); // Baixo (i + 1, j)
+        _check_grid(original, copy_grid(copy), word, index + 1, i, j - 1, found_word); // Esquerda (i, j - 1)
+        _check_grid(original, copy_grid(copy), word, index + 1, i, j + 1, found_word); // Direita (i, j + 1)
     }
 }
 
-char **copy_grid(char **original_grid)
+void check_grid(matrix_t original, vector_t word, bool *found_word)
 {
-    char **copy = (char **) malloc(n * sizeof(char *));
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+            _check_grid(original, copy_grid(original), word, 0, i, j, found_word);
+    }
+}
+
+int main()
+{
+    cin >> n >> m;
+    getchar(); // Consome o '\n'
+
+    vector_t row(m);
+    matrix_t grid;
 
     for (int i = 0; i < n; i++)
     {
-        copy[i] = (char *) malloc(m * sizeof(char));
-
         for (int j = 0; j < m; j++)
-            copy[i][j] = original_grid[i][j];
+            scanf("%c", &row[j]);
+        grid.push_back(row);
+        
+        getchar(); // Consome o '\n'
     }
 
-    return copy;
+    cin >> k;
+    getchar(); // Consome o '\n'
+
+    vector_t word(k);
+    for (int i = 0; i < k; i++)
+        scanf("%c", &word[i]);
+
+    bool found_word = false;
+    check_grid(grid, word, &found_word);
+
+    if (found_word) printf("Yes\n");
+    else printf("No\n");
+
+    return 0;
 }
